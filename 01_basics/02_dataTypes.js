a8b755daccc4abf2d9a07b0ecc3c48dc6fa489f3
Trojan Horse <69629445+trojanhorse-beta@users.noreply.github.com>
2023-12-31 18:13:08 +0000

    datatyprs

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)  we are using Node Js, not browser so this will throw error of not defined
+
+let name = "Trojan"
+let age = 20
+let state = "Delhi"
+let isLoggedIn = false
+
+// dataTypes: number => 2^53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined => value not yet assigned
+// symbol => unique
+
+
+// object 
+
+console.log(typeof null);  //object
+console.log(typeof undefined);   //undefined
