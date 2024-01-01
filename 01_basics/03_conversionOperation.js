072c52ad321ad3c5fddd2d337ffa55bfa0bf6acc
Trojan Horse <69629445+trojanhorse-beta@users.noreply.github.com>
2024-01-01 13:15:06 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = 47
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// null => 0
+// true => 1 , false => 0
+//undefined => NaN
+//string => NaN
+
+let loggedIn = 1
+let booleanLoggedIn = Boolean(loggedIn)
+
+console.log(typeof loggedIn);
+console.log(typeof booleanLoggedIn);
+console.log(booleanLoggedIn);
+
+// 1 => true
+// 0 => false
+// empty string => false
+// string => true
+
+let numb = 23
+let stringNumber = String(numb)
+console.log(stringNumber);
+console.log(typeof stringNumber);
