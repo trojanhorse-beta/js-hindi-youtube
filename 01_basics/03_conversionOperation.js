19c5ecf62702e09e856f400e1d185339ae10f5f1
Trojan Horse <69629445+trojanhorse-beta@users.noreply.github.com>
2024-01-01 13:39:43 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = 47
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+/* console.log(typeof valueInNumber);
+console.log(valueInNumber); */
 
 // "33" => 33
 // null => 0
@@ -16,9 +16,9 @@ console.log(valueInNumber);
 let loggedIn = 1
 let booleanLoggedIn = Boolean(loggedIn)
 
-console.log(typeof loggedIn);
+/* console.log(typeof loggedIn);
 console.log(typeof booleanLoggedIn);
-console.log(booleanLoggedIn);
+console.log(booleanLoggedIn); */
 
 // 1 => true
 // 0 => false
@@ -27,5 +27,22 @@ console.log(booleanLoggedIn);
 
 let numb = 23
 let stringNumber = String(numb)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+/* console.log(stringNumber);
+console.log(typeof stringNumber); */
+
+/*****************Operations *****************/
+
+let str1 = "Hello"
+let str2 = " and Bye"
+let str3 = str1 +str2
+
+/* console.log(str3);
+
+console.log("1" +2);
+console.log(1 + "2");
+console.log("1" +2 +2);
+console.log(1 + "2" + "2");
+console.log(1 + 2 + "2"); */
+
+/* console.log(+true);
+console.log(+""); */
