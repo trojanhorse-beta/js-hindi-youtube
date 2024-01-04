1d93b89c33f648d43a21c30da9457f385711da56
Trojan Horse <69629445+trojanhorse-beta@users.noreply.github.com>
2024-01-04 20:31:44 +0000

    added Stack and Heap to existing file

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -7,3 +7,33 @@
 // Non- primitive (call by reference)
 
 // Array, Objects, functions
+
+
+/******************************************/
+
+// Memory
+
+// Stack(used for primitive type), Heap(used for non-primitive type)
+//Stack Example:
+
+let myYoutubeName = "sampleName" //goes in the stack
+
+let anotherName = myYoutubeName;
+anotherName = "NewName";
+
+//console.log(myYoutubeName);
+//console.log(anotherName);
+
+//Heap Example:
+
+let userOne = {
+    email: "test@test.com",
+    upi: "test@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "updated@test.com"
+
+console.log(userOne);
+console.log(userTwo);
