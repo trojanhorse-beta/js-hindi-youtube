5771e9a3cc187afc592b311ff12590fe452fc57c
Trojan Horse <69629445+trojanhorse-beta@users.noreply.github.com>
2024-01-05 20:44:03 +0000

    Objects part 1

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,36 @@
+// Singleton: jab constructor se object bnate hai to singleton object banta hai. apne tarike ka ek hi object banega
+//Object.create
+
+// object literals
+
+const mySymbol = Symbol("key1")
+
+const jsUser = {
+    name: "Trojan",
+    "full name": "Trojan Horse",   //cannot be access using . notation
+    age: 20,
+    [mySymbol]: "myKey1",
+    location: "Delhi",
+    email: "test@test.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
+}
+
+//console.log(jsUser["full name"]);
+//console.log(jsUser.email);
+//console.log(jsUser);
+//console.log(typeof jsUser[mySymbol]);
+
+//Object.freeze(jsUser)  //freezes that object. The values in that object cannot be updated now.
+
+
+jsUser.greeting = function(){
+    console.log(`Hello ${jsUser["full name"]}`);
+}
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello ${this["full name"]}, you are logged in from ${this.location}`);
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
