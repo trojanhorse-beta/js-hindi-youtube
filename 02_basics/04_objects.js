2d0b8bfb7d0e5fba62307702fe2dcc6ee85f9efc
Trojan Horse <69629445+trojanhorse-beta@users.noreply.github.com>
2024-01-06 16:17:01 +0000

    Added API content to Objects file

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -23,8 +23,37 @@ const obj4 = Object.assign({},obj1,obj2) // joining objects
 const obj3 = {...obj1,...obj2}  // another way of joining objects using Spread operator
 //console.log(obj3);
 
-console.log(Object.keys(tinderUser));    // to get all the keys of a particular Object. Returns an array
-console.log(Object.values(tinderUser));    // to get all the values of a particular Object. Returns an array
-console.log(Object.entries(tinderUser));   // returns key value pairs of the passed Object in an array format
+//console.log(Object.keys(tinderUser));    // to get all the keys of a particular Object. Returns an array
+//console.log(Object.values(tinderUser));    // to get all the values of a particular Object. Returns an array
+//console.log(Object.entries(tinderUser));   // returns key value pairs of the passed Object in an array format
 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));   // to check whether the Object contains that property/key or not
+//console.log(tinderUser.hasOwnProperty("isLoggedIn"));   // to check whether the Object contains that property/key or not
+
+
+//Better Syntax example for clean code to access Object values multiple times (De-Structuring of an Object)
+const course = {
+    courseName: "JS in Hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+//const {courseInstructor} = course
+//console.log(courseInstructor);
+
+const {courseInstructor: anyNameYouWantToUse} = course
+console.log(anyNameYouWantToUse);
+
+//************ API **********//
+
+/* {
+    "name": "Trojan",
+    "courseName": "Js in Hindi",
+    "price": "free"
+} */
+
+//JSON structure example below
+[
+    {},
+    {},
+    {}
+]
