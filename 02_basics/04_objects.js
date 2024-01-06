a6620d78370225f4c53ed7a4f4095fe5986d40e9
Trojan Horse <69629445+trojanhorse-beta@users.noreply.github.com>
2024-01-06 15:42:40 +0000

    More on Objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,30 @@
+const tinderUser = {}
+
+tinderUser.id = "123"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        firstname: "Trojan",
+        lastname: "Horse"
+    }
+}
+
+//console.log(regularUser.fullname?.firstname);   // ? to check whether it has value or not. If it contains value, it will move forward otherwise stop
+
+const obj1 = {1:"a", 2: "b"}
+const obj2 = {3:"c",4:"d"}
+
+const obj4 = Object.assign({},obj1,obj2) // joining objects
+const obj3 = {...obj1,...obj2}  // another way of joining objects using Spread operator
+//console.log(obj3);
+
+console.log(Object.keys(tinderUser));    // to get all the keys of a particular Object. Returns an array
+console.log(Object.values(tinderUser));    // to get all the values of a particular Object. Returns an array
+console.log(Object.entries(tinderUser));   // returns key value pairs of the passed Object in an array format
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));   // to check whether the Object contains that property/key or not
