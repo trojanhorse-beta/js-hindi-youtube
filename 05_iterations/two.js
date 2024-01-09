a89ae0d85758c0824af4ad1699f6f9369dd16f87
Trojan Horse <69629445+trojanhorse-beta@users.noreply.github.com>
2024-01-09 20:09:22 +0000

    while and do-while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// While loop
+
+let index = 0
+while (index <= 10) {
+    //console.log(`VALUE OF INDEX IS ${index}`);
+    index = index+2
+}
+
+
+let myArray = ["flash","batman","superman"]
+let arr = 0
+
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+
+
+// Do- while loop
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score = score +1
+} while (score <= 10);
